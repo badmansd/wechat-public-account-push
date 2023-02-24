25c765e40703e26d3b5d1058cb064a05e154fe31
badmansd <114740777+badmansd@users.noreply.github.com>
2023-02-24 14:04:26 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -51,11 +51,11 @@ USE_PASSAGE: 'push-deer',
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: '0002',
 
   CALLBACK_USERS: [
     {
-      name: '自己',
+      name: 'MangoZ',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o_hEJ6O3xOGC4mUl0QCdXLrHFeUA',
     }
