050cb55e7777ab5c9034ec6cad0835233b399ae2
badmansd <114740777+badmansd@users.noreply.github.com>
2023-02-24 14:37:50 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ USE_PASSAGE: 'push-deer',
       // 想要发送的人的名字
       name: '怹',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o_hEJ6NZls4Bgv9y2iAtx_Rt9gnU',
+      id: 'ac8c1c5a0f224256875bf3189767acb6',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
